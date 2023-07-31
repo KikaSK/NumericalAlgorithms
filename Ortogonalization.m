8af51fe1189e28e0b0cb5e28718405f62b24346e
KikaSK <korecovak@gmail.com>
2023-07-31 23:14:14 +0200

    Add numerical algorithms to Github

---
+++ Ortogonalization.m
@@ -0,0 +1,228 @@
+## discrete orthogonalization of real functions (type="dis")
+## continuous orthogonalization of real functions (type="con")
+
+## After first run of the program we have to wait for a few 
+## seconds until symbolic pagkage loads
+
+## test-case 1: 
+## type: con
+## Number of functions: 4
+## 1
+## x
+## x^2
+## x^3
+## Begining of the interval: -1
+## End of the interval: 1
+## Weight function: 1-x^2
+
+## test-case 2:
+## type: dis
+## Number of functions: 4
+## 1
+## x
+## x^2
+## x^3
+## Points: 0 1 2 3 (press enter after each point)
+## Weight function: 1
+
+## test-case 2:
+## type: con
+## Number of functions: 5
+## 1
+## sin(x)
+## cos(x)
+## sin(2*x)
+## cos(2*x)
+## Begining of the interval: -pi
+## End of the interval: pi
+## Weight function: 1
+
+clear;
+clear -global;
+pkg load symbolic
+
+disp("Input type of ortogonalization: (""con"" for continuous or ""dis"" for discrete)")
+prompt="";
+
+type = input(prompt, 's'); 
+
+while not(strcmp(type, "con") || strcmp(type, "dis"))
+  disp("Wrong input, please input ""con"" or ""dis"": ")
+  prompt="";
+  type = input(prompt, 's');
+endwhile
+
+prompt = "Number of functions: ";
+global n = input(prompt);
+
+while n<2
+  prompt="Please input integer greater than 1: ";
+  n = input(prompt);
+endwhile
+
+my_functions = cell(n,1);
+my_ort_functions = cell(n,1);
+
+disp("Enter all functions as functions of variable x. Functions must be linearly independent!");
+
+## adds @(x) in front of the string, makes it a proper input for "str2func"
+function f = to_function_handle(f1)
+  f = strjoin({"@(x)",f1}, ' ');
+endfunction
+
+## I tried to find out whether input function is in the right format
+## this is the best what I was able to do..
+for i=1:n
+  prompt = strjoin({"Enter function ", int2str(i), ": "},'');
+  
+  while(1)
+    my_functions{i} = input(prompt, 's');
+    try
+      my_f = str2func(to_function_handle(my_functions{i}))(1); ## this line didn't throw error for "fsgrgihg" and similar "functions"
+      if isnan(my_f) ## so I did it like this
+        error();
+      endif
+      break;
+    catch
+      disp("Invalid function, try again: ")
+    end_try_catch
+  endwhile
+  
+endfor
+
+global i_a;
+global i_b;
+global my_points;
+
+if strcmp(type, "con")
+  prompt = "Begining of interval: ";
+  i_a = input(prompt);
+  prompt = "End of interval: ";
+  i_b = input(prompt);
+else
+  disp(strjoin({"Enter", int2str(n), "points: (after each point press enter)"}, ' '))
+  for i=1:n
+    prompt = "";
+    point = input(prompt);
+    my_points(i) = point;
+  endfor
+endif
+
+if strcmp(type, "con")
+  disp("Weight function must be continuous, real and positive on the interval (a,b) and it's integral over the interval must converge!")
+else
+  disp("Weight function must be positive in all points!")
+endif
+
+prompt = "Enter weight function: ";
+global w_str = input(prompt, 's');
+
+## takes 2 strings and returns string representing sum of input strings 
+function f = add_functions(f1, f2) 
+  f=strjoin({f1, "+(", f2, ")"}, ''); 
+endfunction
+
+## takes 2 strings and returns string representing difference of input strings
+function f = sub_functions(f1, f2)
+  f = strjoin({f1, "-(", f2, ")"}, '');
+endfunction
+
+## takes 2 strings and returns string representing multiple of input strings
+function f = mult_functions(f1, f2)
+  f = strjoin({"(", f1, ")*(", f2, ")"}, '');
+endfunction
+
+## takes constant and string and returns string representing multiple of string by constant
+function f = mult(c, f1)
+  f = strjoin({sprintf('%.5f',c), "*(", f1, ")"}, '');
+endfunction
+
+## calculates dot product of two functions in continuous orthogonalization
+function f = cont_scalar(f1, f2)
+  global i_a;
+  global i_b;
+  global w_str;
+  
+  to_integrate = str2func(to_function_handle(mult_functions(mult_functions(f1,f2),w_str)));
+  f = quad(to_integrate, i_a, i_b); ##integrate over the interval
+endfunction
+
+## calculates dot product of two functions in discrete orthogonalization
+function f = disc_scalar(f1, f2)
+  global n;
+  global w_str;
+  global my_points;
+  
+  ## make functions from strings so we can evaluate them in points
+  f1_func = str2func(to_function_handle(f1)); 
+  f2_func = str2func(to_function_handle(f2));
+  w = str2func(to_function_handle(w_str));
+  
+  f=0;
+  for i=1:n ##sum
+    index = my_points(i);
+    f = f+f1_func(index)*f2_func(index)*w(index);
+  endfor
+endfunction
+
+## first function stays the same
+my_ort_functions{1} = my_functions{1};
+
+## formula for orthogonalization
+for i=2:n
+  sum = "0";
+  
+  for j=1:(i-1)  
+    ## use dot product correctly depending on type of orthogonalization
+    if strcmp(type, "dis")
+      scal_up = disc_scalar(my_functions{i}, my_ort_functions{j});
+      scal_down = disc_scalar(my_ort_functions{j}, my_ort_functions{j});
+    else
+      scal_up = cont_scalar(my_functions{i}, my_ort_functions{j});
+      scal_down = cont_scalar(my_ort_functions{j}, my_ort_functions{j});
+    endif
+    
+    ## using my functions to calculate sum and multiplication by constant
+    to_add = mult((scal_up/scal_down), my_ort_functions{j});
+    sum = add_functions(sum, to_add);
+    
+  endfor
+  ## using my function to calculate difference
+  my_ort_functions{i} = sub_functions(my_functions{i}, sum);
+end;
+
+## using the "symbolic" package to simplify functions 
+syms x
+
+## converting my string representation to symbolic expressions
+func = cell2sym(my_ort_functions); 
+
+## displaying result
+disp("")
+disp("Result of calculations:")
+disp("")
+for i=1:n
+  disp(strjoin({"Function ", int2str(i), ":"},''));
+  disp(func(i))
+end;
+
+## checking for errors
+for i=1:n
+  for j=1:(i-1)
+    ## for each pair of functions calculate dot product
+    ## if the dot product is less than some treshold we declare it as a success
+    ## if the dot product is more than the treshold we display it for the manual check
+    if strcmp(type, "dis")
+      error = disc_scalar(my_ort_functions{i}, my_ort_functions{j});
+    else
+      error = cont_scalar(my_ort_functions{i}, my_ort_functions{j});
+    endif
+      
+    if error<0.00001 ## adjust treshold here
+      disp(strjoin({"Function", int2str(i), "and function", int2str(j), "are OK"}, ' ')); 
+    else
+      disp(strjoin({"Function", int2str(i), "and function", int2str(j), "have dot product", sprintf('%.5f',error)},' '));
+    endif
+  endfor
+endfor
+
---
+++ PadeApproximation.m
@@ -0,0 +1,213 @@
+## Pade approximation of a function in neighbourhood of 0
+
+## After first run of the program we have to wait for a few 
+## seconds until symbolic pagkage loads
+
+## test-case 1: (example from lesson)
+## beginning: -2
+## end: 2
+## function: exp(x)
+## degree of numerator: 2
+## degree of denominator: 2
+
+## test-case 2:
+## beginning: -2*pi
+## end: 2*pi
+## function: sin(x)
+## degree of numerator: 3
+## degree of denominator: 4
+
+clear;
+pkg load symbolic ## using symbolic package for representation of functions
+syms x ## variable 
+
+prompt = "Enter beginning of the interval: ";
+try
+  a = input(prompt);
+catch
+  a = 0;  
+end_try_catch
+
+prompt = "Enter end of the interval: ";
+try
+  b = input(prompt);
+catch
+  b = 0;  
+end_try_catch
+
+while(a>=b || a>=0 || b<=0 || not(isreal(a)) || not(isreal(b)))
+  disp("Try again! Beginning of the interval must be negative number and end must be positive!")
+  prompt = "Enter beginning of the interval: ";
+  try
+    a = input(prompt);
+  catch
+    a = 0;  
+  end_try_catch
+
+  prompt = "Enter end of the interval: ";
+  try
+    b = input(prompt);
+  catch
+    b = 0;  
+  end_try_catch
+endwhile
+
+prompt = "Enter function to approximate as a function of variable x: ";
+
+## needed for catching majority of wrong input function
+## adds @(x) in front of the string, makes it a proper input for "str2func"
+function f = to_function_handle(f1)
+  f = strjoin({"@(x)",f1}, ' ');
+endfunction
+
+## please enter e^x as exp(x)
+while(1)
+  input_function = input(prompt, 's');
+  try
+    my_f = str2func(to_function_handle(input_function))(0);
+    if (isnan(my_f) || isinf(my_f)) ## throw error if function is udefined at 0
+      error();
+    endif
+    my_function = sym(input_function);
+    break;
+  catch
+    disp("Invalid function, try again: (function must be defined and finite at 0)")
+  end_try_catch
+endwhile
+
+prompt = "Input m: (degree of numerator polynomial) ";
+m = input(prompt);
+
+prompt = "Input k: (degree of denominator polynomial) ";
+k = input(prompt);
+
+N = m+k; ## index
+
+## create McLaurin polynomial of input function
+my_taylor = taylor(my_function, 'expansionPoint', 0, 'order', N+1);
+
+disp("")
+disp("Taylor expansion of input function: ")
+disp(my_taylor)
+disp("")
+
+[coef, t] = coeffs(my_taylor);
+
+## coeffitients of Taylor expansion sorted by ascending power of x
+j=0;
+M = length(coef);
+c = sym(zeros(1,N+1));
+for i=1:(N+1)
+  if(M-j>0 && t(M-j)==x^(i-1))
+    c(i) = coef(M-j);
+    j = j+1;
+  else
+    c(i) = 0;
+  endif
+endfor 
+
+disp("Coefficients of Taylor expansion: ")
+disp(c)
+disp("")
+disp("I'm computing..")
+disp("")
+
+#{ 
+difference from lessons -> indexing begins with 1
+
+Principle of code:
+
+Block matrix looking like this (for m = 1 and k = 2 -> N = 3) 
+represents our linear equations when we assume b(1) = 1.
+
+          k+2=N+1           m+2..N+1       
+             |               |    |
+  b(2) b(3) b(4)  a(1) a(2) a(3) a(4)  right side
+|------------------------------------|  |-----|
+|  0    0    0  | -1    0    0    0  |  |-c(1)|
+| c(1)  0    0  |  0   -1    0    0  |  |-c(2)|
+| c(2) c(1)  0  |  0    0   -1    0  |  |-c(3)|     
+| c(3) c(2) c(1)|  0    0    0   -1  |  |-c(4)|
+|------------------------------------|  |-----|
+
+We also suppose that b(k+2)..b(N+1) and a(m+2)..a(N+1) 
+are 0 so the coresponding columns are omitted and 
+the matrix can be simplified to the following form:
+
+  b(2) b(3)  a(1) a(2)  right side
+|---------------------|  |-----|
+|  0    0  | -1    0  |  |-c(1)|
+| c(1)  0  |  0   -1  |  |-c(2)|
+| c(2) c(1)|  0    0  |  |-c(3)|
+| c(3) c(2)|  0    0  |  |-c(4)|
+|---------------------|  |-----|
+
+We might notice this matrix can have some problems with
+regularity, so we need to check it in our code.
+
+Matrix is representing system of N+1 linear equations.
+Unknown variables are b(2)...b(k+1) and a(1)..a(m+1). 
+
+Block matrix is composed of 2 matrices with following 
+dimensions:
+
+Left Matrix:  L_m - (N+1)x(k)    
+                                --->  [L_m, R_m] - (N+1)x(N+1)
+Right Matrix: R_m - (N+1)x(m+1)
+#}
+
+L_m = sym(zeros(N+1, k));
+for i=1:(N+1)
+  for j=1:min((i-1), k)
+    L_m(i,j) = c(i-j);
+  endfor
+endfor
+
+R_m = sym(zeros(N+1, m+1));
+for i=1:(m+1)
+  R_m(i,i) = -1 ;
+endfor
+
+my_matrix = [L_m, R_m];
+
+## checking regularity of matrix
+if det(my_matrix) == 0
+  disp("")
+  disp("Sorry, matrix is singular, I can't find solution. I recommend changing m or k.")
+  disp("")
+  return
+endif
+
+if N>4
+  disp("I almost got it..")
+  disp("")
+endif
+
+
+right_side = sym(zeros(N+1, 1)); 
+for i=1:N+1
+  right_side(i,1) = -c(i);
+endfor
+
+## solving the system
+ans_coeffs = my_matrix\right_side;
+
+P_m = sym(0); ## polynomial in numerator
+P_k = sym(1); ## polynomial in denominator
+
+for i=1:k
+  P_k = P_k + ans_coeffs(i)*x^i;
+endfor
+
+for i=(k+1):(N+1)
+  P_m = P_m + ans_coeffs(i)*x^(i-(k+1));
+endfor
+
+disp("Result function: ")
+R_mk = P_m/P_k;
+disp(R_mk)
+
+clf('reset');
+ezplot(R_mk,[a, b]);
+hold on;
+ezplot(my_function, [a, b]); 
